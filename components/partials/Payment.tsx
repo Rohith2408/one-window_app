2d18821b670a09b486a214d2bb65e5e76705b9a6
Rohith2408 <kumarrohith24081999@gmail.com>
2024-11-14 01:05:52 +0530

    added do not close message

--- components/partials/Payment.tsx
+++ components/partials/Payment.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useRef, useState } from "react";
-import { Pressable, Text, View } from "react-native"
+import { Pressable, StyleSheet, Text, View } from "react-native"
 import WebView from "react-native-webview";
 import { store } from "../../store";
 import {  Order, Request, ServerResponse } from "../../types";
-import { compareProducts, getServerRequestURL, serverRequest } from "../../utils";
+import { compareProducts, getDevice, getServerRequestURL, serverRequest } from "../../utils";
 import { getBasket } from "../../constants/basket";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 import { updateOrder } from "../../store/slices/ordersSlice";
@@ -13,15 +13,55 @@ import { Fonts, Themes, appStandardStyles } from "../../constants";
 import { addProduct, addProducts, replaceProducts } from "../../store/slices/productsSlice";
 import { useAppSelector } from "../../hooks/useAppSelector";
 import Transitionview from "../resources/Transitionview";
+import Styledtext from "../resources/Styledtext";
+
+const GeneralStyles=StyleSheet.create({
+ 
+})
+
+const TabStyles=StyleSheet.create({
+  note:{
+    fontSize:18
+  }
+})
+
+const MobileSStyles=StyleSheet.create({
+  note:{
+    fontSize:14
+  }
+})
+
+const MobileMStyles=StyleSheet.create({
+  note:{
+    fontSize:16
+  }
+})
+
+const MobileLStyles=StyleSheet.create({
+  note:{
+    fontSize:16
+  }
+})
+
+const styles={
+  Tab:TabStyles,
+  MobileS:MobileSStyles,
+  MobileM:MobileMStyles,
+  MobileL:MobileLStyles
+}
 
 const Payment = (props:{paymentOrderId:string}) => {
 
   //const paymentData=useRef(getBasket("payment_options")).current;
   const order=useAppSelector((state)=>state.orders.data).find((item)=>item._id==props.paymentOrderId);
+  const sharedInfo=useAppSelector((state)=>state.sharedinfo).data;
+  const personalInfo=useAppSelector((state)=>state.personalinfo).data;
   const profile = useRef({ ...store.getState().personalinfo.data, ...store.getState().sharedinfo.data }).current;
   const [showRazorpayPage, setShowRazorpayPage] = useState(order?.paymentDetails.amount==0?false:true);
+  const [error,setError]=useState<undefined|string>(undefined)
   const dispatch=useAppDispatch()
   const [path,navigate]=useNavigation()
+  const Device=useRef<keyof typeof styles>(getDevice()).current
   
   const paymentOptions = useRef({
     key: 'rzp_test_TldsbrWlP8NUF5',
@@ -31,9 +71,9 @@ const Payment = (props:{paymentOrderId:string}) => {
     description: 'Test Transaction',
     order_id: order?.paymentDetails.razorpay_order_id,
     prefill: {
-      name: profile.firstName + ' ' + profile.lastName,
-      email: profile.email,
-      contact: profile.phone?.countryCode+profile.phone?.number,
+      name: sharedInfo?.firstName + ' ' + sharedInfo?.lastName,
+      email: sharedInfo?.email,
+      contact: sharedInfo?.phone?.countryCode+sharedInfo?.phone?.number,
     },
     theme: {
       color: '#3399cc',
@@ -80,7 +120,9 @@ const Payment = (props:{paymentOrderId:string}) => {
   return (
     <View style={[{ flex: 1},appStandardStyles.screenMarginSmall]}>
       {showRazorpayPage ? (
-        <WebView
+        <View style={{flex:1,gap:15}}>
+          <Styledtext styles={[styles[Device].note,{textAlign:'center',lineHeight:24,fontFamily:Fonts.NeutrifStudio.Regular}]} text="Please do not close the screen or exit the app during or after the payment, you will be automatically redirected after successfull payment!" focusWord="do not close the screen or exit the app"/>
+          <WebView
           style={{ flex: 1 }}
           originWhitelist={['*']}
           source={{
@@ -91,7 +133,13 @@ const Payment = (props:{paymentOrderId:string}) => {
                   <script>
                     var options = ${JSON.stringify(paymentOptions)};
                     options.handler = function (response) {
-                      window.ReactNativeWebView.postMessage(JSON.stringify(response));
+                      window.ReactNativeWebView.postMessage(JSON.stringify({ status: 'success', data: response }));
+                    };
+                    options.modal = {
+                      escape: false,
+                      ondismiss: function() {
+                        window.ReactNativeWebView.postMessage(JSON.stringify({ status: 'cancelled' }));
+                      }
                     };
                     var rzp1 = new Razorpay(options);
                     rzp1.open();
@@ -102,19 +150,35 @@ const Payment = (props:{paymentOrderId:string}) => {
           }}
           onMessage={(event) => {
             console.log("Response from razzzz",event)
-            const data = JSON.parse(event.nativeEvent.data);
-            handlePayment(data); 
-            setShowRazorpayPage(false); 
+            try {
+              const data = JSON.parse(event.nativeEvent.data);
+              if (data.status === 'success') {
+                handlePayment(data.data); 
+                setError(undefined);
+              } 
+              else if (data.status === 'cancelled') {
+                navigate?navigate({type:"RemoveSpecificScreen",payload:{id:"Payment"}}):null
+              } 
+              else if (data.status === 'error') {
+                setError(data.message);
+              }
+            } catch (error) {
+              console.error("Failed to parse message data:", error);
+            } finally {
+              setShowRazorpayPage(false); 
+            }
           }}
           onError={(syntheticEvent) => {
             const { nativeEvent } = syntheticEvent;
           }}
         />
+        </View>
+        
       ) : (
         <Transitionview effect="pan" style={[{flex:1}]}>
           <View style={{flex:1,alignItems:"center",justifyContent:"center",gap:10}}>
-            <Text style={[{fontSize:24,fontFamily:Fonts.NeutrifStudio.Bold,color:"green"}]}>Payment Successfull</Text>
-            <Text style={[{fontSize:14,textAlign:"center",fontFamily:Fonts.NeutrifStudio.Regular,color:Themes.Light.OnewindowPrimaryBlue(0.5)}]}>Order details can be viewed in My Orders section!</Text>
+            <Text style={[{fontSize:24,fontFamily:Fonts.NeutrifStudio.Bold,color:error?"red":"green"}]}>{error?"Payment Failed":"Payment Successfull"}</Text>
+            <Text style={[{fontSize:14,textAlign:"center",fontFamily:Fonts.NeutrifStudio.Regular,color:Themes.Light.OnewindowPrimaryBlue(0.5)}]}>{error?error:"Order details can be viewed in My Orders section!"}</Text>
           </View>
         </Transitionview>
         
