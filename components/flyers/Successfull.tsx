3da1ce8944046b643f92fb68f2f1edc7744691be
Rohith2408 <kumarrohith24081999@gmail.com>
2024-12-07 12:17:40 +0530

    added redirect feature

--- components/flyers/Successfull.tsx
+++ components/flyers/Successfull.tsx
@@ -1,17 +1,19 @@
 import { useEffect, useRef } from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { Pressable, StyleSheet, Text, View } from "react-native"
 import useNavigation from "../../hooks/useNavigation"
 import { getDevice } from "../../utils"
 import { Fonts, Themes, appStandardStyles } from "../../constants"
 import { useAppDispatch } from "../../hooks/useAppDispatch"
 import { setRemoveScreen } from "../../store/slices/removeScreenSlice"
+import { getBasket } from "../../constants/basket"
+
 const GeneralStyles=StyleSheet.create({
     main_wrapper:{
         flex:1,
         paddingTop:20,
         justifyContent:"center",
         alignItems:'center',
-        gap:10
+        gap:15
     }
 })
 
@@ -59,11 +61,12 @@ const styles={
     MobileL:MobileLStyles
 }
 
-const Successfull=(props:{message:string,preventAutoHide?:boolean,hideInterval?:number})=>{
+const Successfull=(props:{message:string,redirect?:{text:string,handler:()=>void},preventAutoHide?:boolean,hideInterval?:number})=>{
 
     const [path,navigate]=useNavigation()
     const Device=useRef<keyof typeof styles>(getDevice()).current
-    const dispatch=useAppDispatch()
+    const dispatch=useAppDispatch();
+    const info=getBasket("success-flyer");
 
     useEffect(()=>{
         if(!props.preventAutoHide)
@@ -75,10 +78,24 @@ const Successfull=(props:{message:string,preventAutoHide?:boolean,hideInterval?:
         }
     },[])
 
+    const redirect=()=>{
+        info.redirect.handler();
+        dispatch(setRemoveScreen({id:"Successfull"}))
+    }
+
+    console.log("Success",props);
+
     return(
         <View style={[GeneralStyles.main_wrapper,appStandardStyles.screenMarginSmall]}>
-            <Text style={[styles[Device].text,{color:"green",fontFamily:Fonts.NeutrifStudio.Bold}]}>Success!</Text>
-            <Text style={[styles[Device].subtext,{textAlign:"center",color:Themes.Light.OnewindowPrimaryBlue(0.4),fontFamily:Fonts.NeutrifStudio.Regular}]}>{props.message}</Text>
+            <Text style={[styles[Device].text,{color:Themes.Light.OnewindowPrimaryBlue(1),fontFamily:Fonts.NeutrifStudio.Bold}]}>Success!</Text>
+            <Text style={[styles[Device].subtext,{textAlign:"center",color:Themes.Light.OnewindowPrimaryBlue(0.5),fontFamily:Fonts.NeutrifStudio.Regular}]}>{props.message}</Text>
+            {
+                info?.redirect
+                ?
+                <Pressable onPress={redirect} style={[appStandardStyles.buttonWrapper]}><Text style={[styles[Device].subtext,appStandardStyles.buttonText]}>{info.redirect.text}</Text></Pressable>
+                :
+                null
+            }
         </View>
     )
 }
